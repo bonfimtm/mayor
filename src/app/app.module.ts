35d6ebc98ffd9ceba5434eb21c7940b5912bccf3
Thiago Bonfim <bonfimtm@gmail.com>
2020-10-18 10:58:45 +0200

    Enable routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,34 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireFunctionsModule } from '@angular/fire/functions';
+import { RouterModule, Routes } from '@angular/router';
 
 import { environment } from '../environments/environment';
 
 /* Components */
+import { HomePageComponent } from './home-page/home-page.component';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { InterviewsComponent } from './interviews/interviews.component';
 import { FaqComponent } from './faq/faq.component';
 import { ContactComponent } from './contact/contact.component';
 import { ParallaxComponent } from './parallax/parallax.component';
-
-/* Services */
-import { ContactService } from './contact.service';
 import { AboutComponent } from './about/about.component';
 import { FooterComponent } from './footer/footer.component';
 import { CarouselComponent } from './carousel/carousel.component';
 
+/* Services */
+import { ContactService } from './contact.service';
+
+const appRoutes: Routes = [
+  { path: '', component: HomePageComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    HomePageComponent,
     InterviewsComponent,
     FaqComponent,
     ContactComponent,
@@ -38,6 +44,7 @@ import { CarouselComponent } from './carousel/carousel.component';
     FormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireFunctionsModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [
     ContactService,
