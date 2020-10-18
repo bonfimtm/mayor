583680542a7687864fd776c5d795df472c02e0f8
Thiago Bonfim <bonfimtm@gmail.com>
2020-10-18 12:02:03 +0200

    Add type annotation to server reponse

--- src/app/contact.service.ts
+++ src/app/contact.service.ts
@@ -2,13 +2,17 @@ import { Injectable } from '@angular/core';
 import { AngularFireFunctions } from '@angular/fire/functions';
 import { Observable } from 'rxjs';
 
+interface ContactMessageResponse {
+  success: boolean;
+}
+
 @Injectable()
 export class ContactService {
 
   constructor(private fns: AngularFireFunctions) {
   }
 
-  makeContact(name: string, email: string, subject: string, content: string): Observable<any> {
+  makeContact(name: string, email: string, subject: string, content: string): Observable<ContactMessageResponse> {
     const callable = this.fns.httpsCallable('onCallSendContactMessage');
     return callable({
       name: name,
